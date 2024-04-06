35cd4daab9ee9ca9aa4de9e0a8edb0e5829c52be
raisaosmanova2003 <tu4F7PRWU@code.edu.az>
2024-04-06 09:57:46 +0400

    js-function

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// ?Task-1 
+// ?Kreditə götürdüyümüz pul və aylıq(necə ay ödəməliyəm)verilir. Pul bərabər hissələrə bölünərək hər ay verilməlidir.
+
+
+// ?Task-2
+// ?Reqemlerden ibaret arrayda duplicate reqemi qaytarin (bir funksiya teyin edirik ve parameter kimi reqemlerden ibaret array qebul edir. Arrayde yalniz bir eded duplicate olacaq) : : 
+
+
+// let arr = [1, 3, 4, 2, 2, 8];
+// function Duplicate(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let yeniElement = arr[i];
+//         for (let j = 0; j < i; j++) {
+//             if (arr[j] === yeniElement) {
+//                 return yeniElement;
+//             }
+//         }
+//     }
+// }
+// console.log(Duplicate(arr)); 
+
+
+// ?Task-3
+// ?Funksiya number fahrenheit parameter qebul eden funskiya yaziriq. Celsiusa convert edib return etsin.
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (5/9) * (fahrenheit - 32);
+    return celsius;
+}
+
+let celsiusValue = fahrenheitToCelsius(150);
+console.log(celsiusValue); 
